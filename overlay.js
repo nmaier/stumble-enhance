3763000733b6f96268a161756ce47728b7fce466
Nils Maier <maierman@web.de>
2011-03-05 22:42:44 +0100

    Compatiblity with latest SU

--- overlay.js
+++ overlay.js
@@ -1,6 +1,6 @@
 addEventListener('load', function() {
 	removeEventListener('load', arguments.callee, true);
-	
+
 	let prefNumStumbles = Application.prefs.get("extensions.stumbleenh.numstumbles", 20);
 	let numStumbles = prefNumStumbles.value;
 	prefNumStumbles.events.addListener(
@@ -9,23 +9,43 @@ addEventListener('load', function() {
 			numStumbles = prefNumStumbles.value;
 		} }
 	);
-	
+
+	let prefEnabled = Application.prefs.get("extensions.stumbleenh.enabled");
+	let enabled = prefEnabled.value;
+	prefEnabled.events.addListener(
+		'change',
+		{ handleEvent: function(aEvent) {
+			let enabled = prefEnabled.value;
+		} }
+	);
+
 	let running = false;
-	let original = clickstumble;
+	let stumble = (window.StumbleGlobals.stumble || window.stumble);
+	let cancel_throttle = (window.StumbleGlobals.cancel_stumble_throttle || window.su_cancel_stumble_throttle);
+
+	let original;
 	function override(evt, todo) {
-		let sub = document.getElementById('su_stumble');
-		let sue = document.getElementById('stumbleenh-button') || {
-			setAttribute: function() {}
-		};
+		if (!enabled) {
+			return original.apply(null, arguments);
+		}
 		if (running) {
 			return;
 		}
 		running = true;
 		todo = todo || numStumbles;
-				
+
+		let sub = document.getElementById('su_stumble') || document.getElementById('stumbleglobals_stumble');
+		sub = sub || {
+			setAttribute: function() {}
+		};
+		let sue = document.getElementById('stumbleenh-button');
+		sue = sue || {
+			setAttribute: function() {}
+		};
+
 		sub.setAttribute('stumbleenh', 'true');
 		sue.setAttribute('stumbleenh', 'true');
-		
+
 		let tt = sub.getAttribute('tooltiptext');
 		sub.setAttribute('tooltiptext', todo.toString());
 		sue.setAttribute('tooltiptext', todo.toString());
@@ -38,25 +58,22 @@ addEventListener('load', function() {
 				sue.removeAttribute('tooltiptext');
 				return;
 			}
-			if (!su_stumble_async_context) {
+			if (!(window.StumbleGlobals.stumble_async_context || window.su_stumble_async_context)) {
 				stumble(true);
-				su_cancel_stumble_throttle();
+				cancel_throttle();
 				--todo;
-				sue.setAttribute('tooltiptext', todo.toString());				
+				sue.setAttribute('tooltiptext', todo.toString());
 			}
 			setTimeout(arguments.callee, 500);
 		})();
 	};
-
-	let prefEnabled = Application.prefs.get("extensions.stumbleenh.enabled");
-	function setOverride() {
-		clickstumble = prefEnabled.value ? override : original;
+	if ("StumbleGlobals" in window) {
+		original = function() StumbleGlobals.clickstumble.apply(StumbleGlobals, arguments);
+		StumbleGlobals.clickstumble = override;
 	}
-	setOverride();
-	prefEnabled.events.addListener(
-		'change',
-		{ handleEvent: function(aEvent) {
-			setOverride();
-		} }
-	);
+	else {
+		original = clickstumble;
+		clickstumble = override;
+	}
+	document.getElementById("stumbleenh-button").addEventListener("command", override, true);
 }, true);
